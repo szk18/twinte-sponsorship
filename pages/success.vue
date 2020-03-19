84a7dac4b6fde398b1c4771f59d3a20c2c1c96bd
szk18 <suzuki.kensuke.tp@alumni.tsukuba.ac.jp>
2020-03-19 13:47:10 +0900

    add: result

--- pages/success.vue
+++ pages/success.vue
@@ -3,6 +3,12 @@
     <h1 class="title">
       ありがとうございました。
     </h1>
+    <span class="has-text-primary amount">
+      ¥{{ amount }}
+    </span>
+    <span>の寄付をいただきました。</span>
+    <p>寄付していただきたお金はTwin:teの運用や開発に使用します</p>
+    <br>
     <p>
       <nuxt-link to="/mypage">
         マイページ
@@ -13,5 +19,23 @@
 
 <script>
 export default {
+  data () {
+    return {
+      // type: '',
+      amount: 0
+    }
+  },
+  created () {
+    // this.type = this.$route.query.type
+    this.amount = this.$route.query.amount
+  }
 }
 </script>
+<style lang="scss" scoped>
+.amount {
+  font-style: normal;
+  font-weight: bold;
+  font-size: 50px;
+  line-height: 72px;
+}
+</style>
